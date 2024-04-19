aeed6f51df6fc818ebd8f59e7422408247955b54
gautam0kr <gautamkr.2409@gmail.com>
2024-04-19 12:47:23 +0530

    save

--- creating_classAndObj.cpp
+++ creating_classAndObj.cpp
@@ -1,31 +1,37 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-class data{
-    private:
+class data
+{
+private:
     int d;
-    public:
-    data(){
-        d=0;
 
+public:
+    data()
+    {
+        d = 0;
     }
 
-    data(int dt){
-        d=dt;
+    data(int dt)
+    {
+        d = dt;
     }
 
-    void show(){
-        cout<<d<<endl;
+    void show()
+    {
+        cout << d << endl;
     }
 
-    data add(data obj){
+    data add(data obj)
+    {
         data tempobj;
-        tempobj.d=obj.d+d;
+        tempobj.d = obj.d + d;
         return tempobj;
     }
 };
 
-int main(){
+int main()
+{
     data d1(12), d2(24), d3;
-    d3=d1.add(d2);
+    d3 = d1.add(d2);
     d3.show();
 }
